eb61487eb280184190f2252a9da0b1db80a7d004
raditya <gusrizkydharmaputra123@gmail.com>
2022-10-06 14:08:33 +0800

    learn option in tconfig

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 function add(n1: number, n2: number, type: string) {
-  let result;
+  let result: string;
   if (type === "addition") {
     return (result = `The result of the ${type} of ${n1} and ${n2} is ${
       n1 + n2
