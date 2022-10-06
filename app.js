58c4b4ed77690f178a24349342bd5f0efe8c4bd0
raditya <gusrizkydharmaputra123@gmail.com>
2022-10-06 11:22:41 +0800

    not refactored yet

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("Starting server");
+var TYPE;
+(function (TYPE) {
+    TYPE[TYPE["addition"] = 0] = "addition";
+    TYPE[TYPE["subtraction"] = 1] = "subtraction";
+    TYPE[TYPE["multiplication"] = 2] = "multiplication";
+    TYPE[TYPE["division"] = 3] = "division";
+})(TYPE || (TYPE = {}));
+function add(n1, n2, type) {
+    let result = `The result of the ${type} is ${n1 + n2}`;
+    return result;
+}
--- app.ts
+++ app.ts
@@ -1,1 +1,23 @@
-console.log("Starting server");
+function add(n1: number, n2: number, type: string) {
+  let result;
+  if (type === "addition") {
+    return (result = `The result of the ${type} of ${n1} and ${n2} is ${
+      n1 + n2
+    }`);
+  } else if (type === "substractions") {
+    return (result = `The result of the ${type} of ${n1} and ${n2} is ${
+      n1 - n2
+    }`);
+  } else if (type === "division") {
+    return (result = `The result of the ${type} of ${n1} and ${n2} is ${
+      n1 / n2
+    }`);
+  } else if (type === "multiplication") {
+    return (result = `The result of the ${type} of ${n1} and ${n2} is ${
+      n1 * n2
+    }`);
+  }
+  return;
+}
+
+console.log(add(12, 15, "division"));
